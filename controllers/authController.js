898e3e3d1df189a388f896027a3106689b4bb417
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-08-27 16:27:15 -0500

    created appError class and catchAsync function to reuse them

---
+++ controllers/authController.js
@@ -0,0 +1,10 @@
+const User = require("../models/userModel");
+
+exports.signup = catchAsync(async (req, res, next) => {
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+});
